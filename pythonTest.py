df1df141b48110b51b16114798f97aed8b02c1a3
zcx <zcxyun@126.com>
2017-08-29 17:30:01 +0800

    update pythonTest.py

--- pythonTest.py
+++ pythonTest.py
@@ -1,4 +1,10 @@
-import os
-L = [('Bob', 88), ('Adam', 88), ('Bart', 88), ('Lisa', 88),]
-filelist = [f for f in os.listdir('.')]
-print(filelist)
+##import os
+##L = [('Bob', 88), ('Adam', 88), ('Bart', 88), ('Lisa', 88),]
+##filelist = [f for f in os.listdir('.')]
+##print(filelist)
+from functools import reduce
+def add(x,y):
+    return x + y
+print(list(range(10))[::2])
+print(reduce(add, range(10)[::2]))
+

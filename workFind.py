fb51280fb4fa111a1ac1cb5359776d7486dbe3d7
zcx <zcxyun@126.com>
2017-09-11 11:53:19 +0800

    add workFind.py

--- workFind.py
+++ workFind.py
@@ -1,13 +1,3 @@
-##import os
-##L = [('Bob', 88), ('Adam', 88), ('Bart', 88), ('Lisa', 88),]
-##filelist = [f for f in os.listdir('.')]
-##print(filelist)
-# from functools import reduce
-# def add(x,y):
-#     return x + y
-# print(list(range(10))[::2])
-# print(reduce(add, range(10)[::2]))
-
 # 根据今天什么班计算未来某一天是什么班
 import time
 def workday(someworkOfToday, someday):

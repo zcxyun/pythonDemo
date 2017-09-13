c6ef6c15056ee87b53a58ac120aff303c84a4dca
zcxyun <zcxyun@126.com>
2017-09-13 23:22:47 +0800

    update ooTest.py add test Enum

--- ooTest.py
+++ ooTest.py
@@ -273,17 +273,35 @@
 
 # test __call__
 # 任何类，只需要定义一个__call__()方法，就可以直接对实例进行调用
-class Student(object):
-    def __init__(self, name):
-      self._name = name
-    # def __call__(self):
+# class Student(object):
+#     def __init__(self, name):
+#       self._name = name
+#     # def __call__(self):
     #   print('my name is %s' % self._name)
-s = Student('zcx')
+# s = Student('zcx')
 # print(s())
-print(callable(s))
-print(callable(lambda x: x))
-print(callable(max))
-print(callable([1,2,3]))
-print(callable(None))
-print(callable('str'))
+# print(callable(s))
+# print(callable(lambda x: x))
+# print(callable(max))
+# print(callable([1,2,3]))
+# print(callable(None))
+# print(callable('str'))
 
+# 使用枚举类
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+for name, member in Month.__members__.items():
+  print(name, '=>', member, ',', member.value)
+
+#unique
+class Weekday(Enum):
+  Sun = 0
+  Mon = 1
+  Tue = 2
+  Wed = 3
+  Thu = 4
+  Fri = 5
+  Sat = 6
+for name, member in Weekday.__members__.items():
+  print(name, '=>', member, ',', member.value)

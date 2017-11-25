970f9dec8965ada2ace21faa03382bee09cd7e08
officeRightComputer <zcxyun@126.com>
2017-11-26 01:28:38 +0800

    从csv 文件中读取数据到 excel 中

--- openpyxl_test/csv_test.py
+++ openpyxl_test/csv_test.py
@@ -2,10 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import csv
-# import codecs
-# with codecs.open('resource/example.csv', 'r', 'utf-8') as originFile:
-with open('origin.csv', encoding='utf-8') as originFile:
-  originReader = csv.reader(originFile, delimiter=' ')
+import openpyxl
+# from openpyxl.utils import get_column_letter, column_index_from_string
+
+##with open('resource/example.csv', encoding='utf-8') as originFile:
+with open('origin.csv', encoding='utf-16') as originFile:
+  originReader = csv.reader(originFile, delimiter='\t')
   originData = list(originReader)
-  print(originData)
+##  print(originData)
+  # for row in originReader:
+    # print("Row #" + str(originReader.line_num) + " " + str(row))
+
+wb = openpyxl.load_workbook('test.xlsx')
+ws = wb.get_sheet_by_name('Sheet1')
+##for rowOfCellObjects in ws['B2':'H21']:
+##  for cellObj in rowOfCellObjects:
+##    print(cellObj.coordinate, cellObj.value)
+##  print('--- END OF ROW ---')
+
+ws['B23'].value = originData[1][1]
+ws['C23'].value = originData[1][4]
+ws['D23'].value = originData[1][0]
+ws['E23'].value = originData[1][2]
+ws['F23'].value = ws['B23'].value
+ws['G23'].value = ws['D23'].value
+
+wb.save('test3.xlsx')
 
--- openpyxl_test/excel_test.py
+++ openpyxl_test/excel_test.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import openpyxl
-from openpyxl.utils import get_column_letter, column_index_from_string
+# from openpyxl.utils import get_column_letter, column_index_from_string
 
 # 用openpyxl模块打开Excel文档
-wb = openpyxl.load_workbook('resource/test.xlsx')
+# wb = openpyxl.load_workbook('resource/test.xlsx')
+wb = openpyxl.load_workbook('test.xlsx')
 # print(type(wb))
 # print(wb.get_sheet_names())
 # 从工作簿中取得工作表
+# sheet1 = wb.get_sheet_by_name('Sheet1')
 sheet1 = wb.get_sheet_by_name('Sheet1')
 # print(sheet)
 # print(type(sheet))
@@ -29,15 +31,15 @@ sheet1 = wb.get_sheet_by_name('Sheet1')
   # print(i, sheet.cell(row=i, column=2).value)
 
 # 列字母和数字之间的转换
-print(get_column_letter(1))
-print(get_column_letter(2))
-print(get_column_letter(27))
-print(get_column_letter(900))
-print(column_index_from_string('A'))
-print(column_index_from_string('AA'))
+# print(get_column_letter(1))
+# print(get_column_letter(2))
+# print(get_column_letter(27))
+# print(get_column_letter(900))
+# print(column_index_from_string('A'))
+# print(column_index_from_string('AA'))
 
 # 从表中取得行和列
-for rowOfCellObjects in sheet1['A1':'C3']:
+for rowOfCellObjects in sheet1['B2':'H21']:
   for cellObj in rowOfCellObjects:
     print(cellObj.coordinate, cellObj.value)
   print('--- END OF ROW ---')

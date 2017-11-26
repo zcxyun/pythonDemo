391ffadb2c1dd14aa3290224da8b86e27aee515b
zcx <zcxyun@126.com>
2017-11-26 23:32:51 +0800

    update csv_test.py

--- openpyxl_test/csv_test.py
+++ openpyxl_test/csv_test.py
@@ -5,27 +5,55 @@ import csv
 import openpyxl
 # from openpyxl.utils import get_column_letter, column_index_from_string
 
+money = []
+payee = []
+payeeNumber = []
+firstPayDate = []
+
 ##with open('resource/example.csv', encoding='utf-8') as originFile:
 with open('origin.csv', encoding='utf-16') as originFile:
   originReader = csv.reader(originFile, delimiter='\t')
   originData = list(originReader)
+
+  for index, title in enumerate(originData[0]):
+    if title == '金额':
+      for m in originData[1: len(originData)+1]:
+        money.append(float(''.join(m[index].split(','))))
+    if title == '收款人名称':
+      for p in originData[1: len(originData)+1]:
+        payee.append(p[index])
+    if title == '收款人账号':
+      for pn in originData[1: len(originData)+1]:
+        payeeNumber.append(pn[index])
+    if title == '初次委托日期':
+      for fpd in originData[1: len(originData)+1]:
+        firstPayDate.append(fpd[index])
+# print(money)
+# print(payee)
+# print(payeeNumber)
+# print(firstPayDate)
+
 ##  print(originData)
   # for row in originReader:
     # print("Row #" + str(originReader.line_num) + " " + str(row))
 
 wb = openpyxl.load_workbook('test.xlsx')
 ws = wb.get_sheet_by_name('Sheet1')
-##for rowOfCellObjects in ws['B2':'H21']:
-##  for cellObj in rowOfCellObjects:
-##    print(cellObj.coordinate, cellObj.value)
-##  print('--- END OF ROW ---')
+i = 0
+for rowOfCellObjects in ws['B2':'H31']:
+  for index, cellObj in enumerate(rowOfCellObjects):
+    if cellObj.value == None:
+      if index == 0:
+        cellObj.value = money[0]
+    # for cellObjIndex in len(rowOfCellObjects):
+    # if cellObjIndex == 1:
+    # ws[cellObjIndex] = money[]
+# ws['B23'].value = originData[1][1]
+# ws['C23'].value = originData[1][4]
+# ws['D23'].value = originData[1][0]
+# ws['E23'].value = originData[1][2]
+# ws['F23'].value = ws['B23'].value
+# ws['G23'].value = ws['D23'].value
 
-ws['B23'].value = originData[1][1]
-ws['C23'].value = originData[1][4]
-ws['D23'].value = originData[1][0]
-ws['E23'].value = originData[1][2]
-ws['F23'].value = ws['B23'].value
-ws['G23'].value = ws['D23'].value
-
-wb.save('test3.xlsx')
+wb.save('test.xlsx')
 
